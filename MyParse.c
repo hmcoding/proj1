055fbcf335658bd7dbc8429234c96f2c8561dab4
hmcoding <32525204+hmcoding@users.noreply.github.com>
2018-02-13 04:08:22 -0500

    Update MyParse.c

--- MyParse.c
+++ MyParse.c
@@ -24,7 +24,6 @@ char** ParseInput(char* input)
 	
 	char** split_args = ParseArguments(input);
 	
-	// check for leading '&' and remove if present
 	if (split_args[0] != NULL)
 	{
 		if (strcmp(split_args[0], "&") == 0)
@@ -37,8 +36,7 @@ char** ParseInput(char* input)
 	
 	split_args = ResolvePaths(split_args);
 	
-	// debug message
-	//PrintArgVector(split_args);
+	
 	
 	free(input);
 	
@@ -51,7 +49,6 @@ char* ParseWhitespace(char* line)
 	int whitespace_count = 0;
 	char cur_char = line[it];
 	
-	// delete leading whitespace
 	while (cur_char != '\0' && (cur_char == ' ' || cur_char == '\t' || cur_char == '\n'))
 	{
 		cur_char = line[++it];
@@ -60,23 +57,14 @@ char* ParseWhitespace(char* line)
 	if (it > 0)
 		line = DelFunc(line, it-whitespace_count, it-1);
 	
-	// check for empty string
-	//if (strcmp(line,"")==0)
-	//	return line;
 	
-	// debug message
-	/*printf("Made it through leading whitespace and deleted %i characters...\n",
-		whitespace_count);
-	printf(line);
-	printf("\n");*/
 	
 	it = 0;
 	whitespace_count = 0;
 	
-	// need flag to determine if currently operating on
-	// trailing whitespace
+	
 	int contains_trailing = 0;
-	// delete intermediate extra whitespace
+	
 	while (cur_char != '\0')
 	{
 		cur_char = line[it++];
@@ -104,16 +92,6 @@ char* ParseWhitespace(char* line)
 		whitespace_count = 0;
 	}
 	
-	// debug message
-	/*printf("Made it through intermediate whitespace...\n",
-		whitespace_count);
-	printf(line);
-	printf("%i",strlen(line));
-	printf("\n");*/
-	
-	// bug on return
-	// current guess is somewhere writing over the return address
-	// (buffer overflow)
 	return line;
 }
 
@@ -157,18 +135,16 @@ char** ParseArguments(char* input)
 
 char** ResolvePaths(char** args)
 {
-	int arg_it = 0;		// iterator
-	int cmd_type = 0;	// parent cmd type
-	//int cur_type;		// current type of args[arg_it]				[FIXED COMPILER WARNING UNUSED VAR]
-	int cmd_it = 0;		// it to point to current command
-	int new_cmd = 1;	// flag for new cmd (after | < or >)
+	int arg_it = 0;		
+	int cmd_type = 0;	
+	int cmd_it = 0;		
+	int new_cmd = 1;	
 	char* cur_cmd = args[0];
 
 
 	while(args[arg_it] != NULL)
 	{
-		//cur_type = CmdCheck(args,arg_it);						[FIXED COMPILER WARNING UNUSED VAR]
-		//printf("%s	%i\n", args[arg_it], cur_type);
+		
 		if (new_cmd == 1)
 		{
 			cmd_type = CmdCheck(args, arg_it);  
@@ -220,7 +196,7 @@ char** ResolvePaths(char** args)
 
 		}
 
-		// etime and limits
+		
 		else if (cmd_type == 3)
 		{
 			
@@ -238,12 +214,12 @@ char** ResolvePaths(char** args)
 						args[arg_it] = PathFromEVar(args[arg_it]);
 					}
 
-					//args[arg_it] = PathMaker(args[arg_it]);
+					
 				}
 			}
 		}
 
-		// external commands
+		
 		else if (cmd_type == 1)
 		{
 			if (arg_it == cmd_it)
@@ -270,7 +246,7 @@ char** ExpandVariables(char** args)
 {
 	size_t arg_it = 0;
 	size_t str_it = 0;
-	//char* str = args[arg_it];
+	
 
 	while (args[arg_it] != NULL)
 	{
@@ -286,8 +262,7 @@ char** ExpandVariables(char** args)
 				if (c == '\0' || c == '$')
 				{
 					free(env_var);
-					// $ at end of string
-					// or two $ in a row
+					
 					break;
 				}
 				env_var[0] = c;
@@ -303,11 +278,10 @@ char** ExpandVariables(char** args)
 				if (ret_env == NULL)
 				{
 					free(env_var);
-					// invalid env variable
 					break;
 				}
 				args[arg_it] = CharRep(args[arg_it], str_it - count - 1, str_it - 1, ret_env);
-				// must update iterator since string was changed
+				
 				str_it = str_it + strlen(env_var);
 				free(env_var);
 			}
