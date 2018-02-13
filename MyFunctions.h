a1428ba68e369eaaf8946604bb27581dd3630c7b
hmcoding <32525204+hmcoding@users.noreply.github.com>
2018-02-13 13:54:44 -0500

    Update MyFunctions.h

--- MyFunctions.h
+++ MyFunctions.h
@@ -3,96 +3,42 @@
 #include<stdlib.h>
 #include<sys/stat.h>
 
-// Big free
-// Deallocates the memory in a 2D character array
-void MemFunc(char** arr);
 
-// PrintArgVector
-// Prints out each argument on a separate line
 void DisplayArgs(char** args);
 
-// DeleteCharacter
-// Deletes a series of characters from a dynamic character array
-// The start and end iterations are inclusive
-char* DelFunc(char* line, size_t start, size_t end);
 
-// FileExist
 int ExistCheck(const char* filename);
-
-// IsExecutable
 int ExecCheck(const char* filename);
-
-// DynStrCat
-// This function frees dest and returns the new
-// string with the format (dest, source)
-char* LinkString(char* x, const char* y);
-
-// DynStrPushBack
-// This functions pushes a character onto the back of a dynamic
-// string
-char* BPushString(char* x, char y);
-
-// DynStrPushFront
-// This functions pushes a character onto the front of a dynamic
-// string
-char* FPushString(char* x, char y);
-
-// RemoveLastDir
-char* RmDir(char* p);
-
-// IsCommand
 int CmdCheck(char** args, int a);
-
-// BuildPath
-// Builds a path by resolving '.' '..' and '~'
-char* PathMaker(char* s);
-
-// PathEnvBuildPath
-// Builds a path by first grabbing beginning of full path from the path env variable
-char* PathFromEVar(char* s);
-
-// ValidExternalCommand
-int CmdVal(const char* s);
-
-// CleanPathDirectory
-// Removes the '/' at the end of a directory if there is one
-char* PathClear(char* s);
-
-// ReplaceSubStr
-// Replaces the characters between iterators start and end (inclusive), with
-// the string contained in source
-char* CharRep(char* x, size_t start, size_t end, const char* y);
-
-// ContainsChar
-// Checks if a string contains a character
 int CharCheck(const char* s, char c);
-
-// VecContainsStr
-// Checks if an arg vector contains the string, and returns the first
-// location of the string
-// Returns -1 if the character does not exist in the vector
 int StringCheck(char** argv, const char* s);
 
-// ArraySize
-// Computes the size (not including NULL) of an arg vector
-int GetSize(char** argv);
 
-// ArrayPushBack
-// Pushes an argument onto the back of an argv
+char* BPushString(char* x, char y);
+char* FPushString(char* x, char y);
 char** PBackArr(char** argv, const char* s);
 
-// ArrayRemoveElement
-// Removes an element from an argv
+char* RmDir(char* p);
+char* DelFunc(char* line, size_t start, size_t end);
+char* CharRep(char* x, size_t start, size_t end, const char* y);
 char** RemoveArr(char** argv, int a);
 
-// ArgvToString
-// Prints an argv to a string and returns the string
-// The caller of this function is responsible for freeing memory
-char* Convert(char** argv);
+char* PathMaker(char* s);
+char* PathClear(char* s);
+char* PathFromEVar(char* s);
 
-// ArgvCountSymbol
-// Counts the number of times a string appears in an argument vector
+
+int CmdVal(const char* s);
+int GetSize(char** argv);
+char* Convert(char** argv);
 int CountStr(char** argv, const char* s);
+
+
+char* LinkString(char* x, const char* y);
+
+void MemFunc(char** arr);
+
+
 
 
 
